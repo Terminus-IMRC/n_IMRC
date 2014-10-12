83dbeeda69f1f19c98b45ee7bfdf1c22d6f9588f
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-10-12 17:47:22 +0900

    add main extention for ttytter

---
+++ ext-main.pl
@@ -0,0 +1,27 @@
+use utf8;
+
+$handle=sub{
+	$flag=0;
+	$ref=shift;
+
+	# get screen_name of the tweet
+	$sn=&descape($ref->{'user'}->{'screen_name'});
+	$sn=~s#/#-#g;
+
+	# return if the tweet is by me
+	return 0 if ("${sn}" eq 'n_IMRC');
+
+	$dtext=&descape($ref->{'text'});
+
+	if("${dtext}" eq 'Yo'){
+		$str="Yo";
+		&updatest($str);
+		$flag=1;
+	}
+
+	if("${flag}" eq 1){
+		&defaulthandle($ref);
+	}
+
+	return 1;
+};
